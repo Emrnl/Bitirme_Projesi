bd789eff6b25c95a3a3433d27fdf81eb730926c7
emrnl <emreunal2@gmail.com>
2018-03-10 15:02:57 +0300

    hello
    Bölüm Denemesi ilk

---
+++ Assets/Scripts/Player_ItemInteraction.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+// StunObject Tag = StunCollectible
+// Player object needs a proper rigidbody
+
+public class Player_ItemInteraction : MonoBehaviour {
+
+	public GameObject O_PlayerObject;
+	public GameObject O_StunCollectible;
+	public int Int_CollectibleCount;
+	public Text T_StunCounter;
+	// Use this for initialization
+	void Awake () 
+	{
+		O_PlayerObject = gameObject;
+		Int_CollectibleCount = 0;
+		TextController ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "StunCollectible") // Change Tag Here!!! 
+		{
+			Int_CollectibleCount += 1;
+			TextController ();
+			Destroy (other.gameObject);
+		}
+
+	}
+
+	void UseStunCollectible()
+	{
+		if (Int_CollectibleCount >= 1) 
+		{
+			Int_CollectibleCount -= 1;
+			TextController ();
+		} 
+		else 
+		{
+			Debug.Log ("No Ammo"); 
+		}
+	}
+
+	void TextController()
+	{
+		if (Int_CollectibleCount > 0)
+		T_StunCounter.text = "Darts: " + Int_CollectibleCount.ToString ();
+		else
+		T_StunCounter.text = "Out of Ammo";
+	}
+
+
+}
+
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+
+
+public class TimeManager : MonoBehaviour {
+
+	public float Time_TimeRemain;
+	public Text TimeText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Time_TimeRemain -= Time.deltaTime;
+		TimeText.text = "" +Mathf.Round(Time_TimeRemain).ToString ();
+        TimeText.color = new Color32(26, 38, 118, 255);
+	}
+}
