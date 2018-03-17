cedf99a040a342b99044355e5f333df5b01f41a3
emrnl <emreunal2@gmail.com>
2018-03-17 18:53:05 +0300

    Ai deneme scenei başarıyla çalıştı levelda düzenleme oldu ai script karakter movement script hazırlandı animasyonlar editlendi düzenlendi (bi sonraki committe kamera follow düzenlenecek)

---
+++ Assets/Scripts/SmoothFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class SmoothFollow : MonoBehaviour
+{
+
+
+	Vector3 offset;
+
+	void Start()
+	{
+		offset = Camera.main.transform.position - this.transform.position;
+
+	}
+
+
+	void Update()
+	{
+		Camera.main.transform.position = this.transform.position + offset;
+	}
+		
+
+}
