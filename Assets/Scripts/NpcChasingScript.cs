3b88100f377490e82131b1b80dd13b19511c8ff2
emrnl <emreunal2@gmail.com>
2018-03-28 16:41:19 +0300

    AI üzerine çalışma state machine ile ai denemeleri 1

--- Assets/Scripts/NpcChasingScript.cs
+++ Assets/Scripts/NpcChasingScript.cs
@@ -7,7 +7,17 @@ public class NpcChasingScript : MonoBehaviour {
 	public Transform head;
 	static Animator anim;
 	bool pursuing = false;
+	public float distanceToPlayer = 3f;
 
+
+	public enum AIState
+	{
+		OnPatrol,
+		OnPursuing,
+		OnAttack,
+		OnIdle
+
+	}
 
 	// Use this for initialization
 	void Start () 
@@ -18,18 +28,25 @@ public class NpcChasingScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+		RaycastHit hit;
+		Ray headRay = new Ray (transform.position, head.forward); // headten ray atıyorum
+
 		Vector3 direction = player.position - this.transform.position;
 		direction.y = 0;
 		
 		float angle = Vector3.Angle(direction, head.forward);
 
-		if(Vector3.Distance(player.position, this.transform.position) < 10 && (angle < 30 || pursuing))
-		{
-			
-			pursuing = true;
-			this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
-										Quaternion.LookRotation(direction), 0.1f);
 
+		if(Vector3.Distance(player.position, this.transform.position) < 10 && (angle < 30 || pursuing) )  // eğer görebiliyorsam
+		{
+			if (Physics.Raycast (headRay, out hit, distanceToPlayer)) {  // şurayı bi kontrol et tekrar düzgün çalışmıyo
+				if (hit.collider.tag == "Player") {
+				}
+				pursuing = true;
+				this.transform.rotation = Quaternion.Slerp (this.transform.rotation,
+					Quaternion.LookRotation (direction), 0.1f);
+			
+			}
 			anim.SetBool("isIdle",false);
 			if(direction.magnitude > 5)
 			{
@@ -37,7 +54,7 @@ public class NpcChasingScript : MonoBehaviour {
 				anim.SetBool("isWalking",true);
 				anim.SetBool("isAttacking",false);
 			}
-			else
+			else 
 			{
 				anim.SetBool("isAttacking",true);
 				anim.SetBool("isWalking",false);
