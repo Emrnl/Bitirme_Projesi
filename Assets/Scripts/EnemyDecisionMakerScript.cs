409c17ba19302c100da56943edbdee16dfd797e5
emrnl <emreunal2@gmail.com>
2018-04-22 13:58:38 +0300

    navmeshle hareket devam

---
+++ Assets/Scripts/EnemyDecisionMakerScript.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDecisionMakerScript : MonoBehaviour {
+
+
+	public float patrolSpeed = 20f;
+	public float chaseSpeed = 30f;
+	public float chaseWaitTime = 50f;
+	public float patrolWaitTime = 10f;
+	public Transform[] patrolWaypoints;
+
+	private EnemySightScript enemySight;
+	private UnityEngine.AI.NavMeshAgent nav;
+	private Transform player;
+	private EnemySightScript lastSeenPos;
+	private float chaseTimer;
+	private float patrolTimer;
+	private int wayPointIndex;
+
+
+	void Awake ()
+	{
+		enemySight = GetComponent<EnemySightScript> ();
+		nav = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+
+	}
+
+
+	void Update()
+	{
+		if (enemySight.playerInSight) {
+
+			Chasing ();
+
+		} else
+
+			Patrolling ();
+	}
+
+
+	void Chasing()
+	{
+		Debug.Log ("chasing");
+
+		Vector3 sightingDeltaPos = enemySight.playerLastSeenPos - transform.position;
+
+		if (sightingDeltaPos.sqrMagnitude < 4) {
+			nav.destination = enemySight.playerLastSeenPos;
+			nav.speed = chaseSpeed;
+
+			if (nav.remainingDistance < nav.stoppingDistance) 
+			{
+				chaseTimer += Time.deltaTime;
+
+				if (chaseTimer >= chaseWaitTime) 
+				{
+					lastSeenPos.playerLastSeenPos = lastSeenPos.resetPosition;
+					enemySight.personalLastSighting = lastSeenPos.resetPosition;
+					chaseTimer = 0f;
+				}
+			}
+		} else
+			chaseTimer = 0f;
+	}
+
+	void Patrolling()
+	{
+
+		Debug.Log ("patrolling");
+
+		nav.speed = patrolSpeed;
+
+		if (nav.destination == lastSeenPos.resetPosition || nav.remainingDistance < nav.stoppingDistance) 
+		{
+			patrolTimer += Time.deltaTime;
+
+			if (patrolTimer >= patrolWaitTime) 
+			{
+				if (wayPointIndex == patrolWaypoints.Length - 1)
+					wayPointIndex = 0;
+				else
+					wayPointIndex++;
+
+				patrolTimer = 0;
+			}
+		} else
+			patrolTimer = 0;
+
+		nav.destination = patrolWaypoints [wayPointIndex].position;
+
+	}
+		
+}
---
+++ Assets/Scripts/EnumAIScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnumAIScript : MonoBehaviour {
+
+	public Transform player;
+	public bool playerInSight;
+	public float fieldOfViewAngle = 110f;
+	public SphereCollider col;
+
+	public enum GameState 
+	{
+		Patrol,
+		Chase,
+		Search,
+		Attack
+	}
+
+	public GameState state;
+	public Vector3 lastSeenPos;
+	public Transform[] waypoints;
+	private NavMeshAgent agent;
+	public int currentWp = 0;
+	public float accuracyWp = 2f;
+	public float speed = 2f;
+	public float rotSpeed = 1f;
+
+
+	void Start()
+	{
+		state = GameState.Patrol;
+		agent = GetComponent<NavMeshAgent> ();
+
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			playerInSight = false;
+
+			Vector3 direction = other.transform.position - transform.position;
+			float angle = Vector3.Angle (direction, transform.forward);
+
+			if(angle < fieldOfViewAngle * 0.5f)
+			{
+				RaycastHit hit;
+
+				if (Physics.Raycast (transform.position + transform.up, direction.normalized, out hit, col.radius)) 
+				{
+					playerInSight = true;
+					state = GameState.Search;
+				}
+			}
+		}
+	}
+
+	void Update()
+	{
+
+		if (state == GameState.Patrol && waypoints.Length > 0) 
+		{
+			agent.destination = waypoints [currentWp].position;
+			//currentWp = (currentWp + 1) % waypoints.Length;
+			if (agent.remainingDistance <= 0)
+			{
+				currentWp++;
+				if (currentWp >= waypoints.Length) 
+				{
+					currentWp = 0;
+				}
+			}
+
+
+		}
+
+	}
+
+
+
+
+}
