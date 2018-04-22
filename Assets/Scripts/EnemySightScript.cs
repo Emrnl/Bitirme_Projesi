fddbf6ad08995784ea8c13e25cf25fa6c4797382
emrnl <emreunal2@gmail.com>
2018-04-22 11:28:29 +0300

    Ai artık duvarlardan görmüyor

---
+++ Assets/Scripts/EnemySightScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySightScript : MonoBehaviour {
+
+	//public Vector3 position = new Vector3(1000f,1000f,1000f);
+	//public Vector3 resetPosition = new Vector3 (1000f,1000f,1000f);
+
+	public float fieldOfViewAngle = 110f;
+	public bool playerInSight;
+	public Vector3 personalLastSighting;
+	public Transform head;
+
+	private UnityEngine.AI.NavMeshAgent nav;
+	public SphereCollider col;
+	public Animator anim;
+	//public DoneLastPlayerSighting lastPlayerSighting;
+	public GameObject player;
+	public Animator playerAnim;
+	public Vector3 previousSighting;
+
+
+	void Awake()
+	{
+		nav = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		//col = GetComponent<SphereCollider> ();
+		anim = GetComponent<Animator> ();
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerAnim = player.GetComponent<Animator> ();
+
+	//	personalLastSighting = lastPlayerSighting.resetPosition;
+	//	previousSighting = lastPlayerSighting.resetPosition;
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		if (other.gameObject == player) 
+		{
+			playerInSight = false;
+
+			Vector3 direction = other.transform.position - transform.position;
+			float angle = Vector3.Angle (direction, transform.forward);
+
+			if(angle < fieldOfViewAngle * 0.5f)
+			{
+				RaycastHit hit;
+
+				if (Physics.Raycast (transform.position + transform.up, direction.normalized, out hit, col.radius)) 
+				{
+					playerInSight = true;
+				}
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/NpcAIChasingScript.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NpcAIChasingScript : MonoBehaviour {
+
+	public Transform player;
+	public Transform head;
+	Animator anim;
+	public float distanceToPlayer = 3f;
+
+	string state = "patrol";
+	public GameObject[] waypoints;
+	int currentWp = 0;
+	float rotSpeed = 0.5f;
+	float speed = 3f;
+	float accuracyWp = 5.0f;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		Vector3 direction = player.position - this.transform.position;
+		direction.y = 0;
+		float angle = Vector3.Angle (direction, head.forward);
+		
+		if (state == "patrol" && waypoints.Length > 0) 
+		{
+			anim.SetBool ("isIdle", false);
+			anim.SetBool ("isWalking", true);
+			if (Vector3.Distance (waypoints [currentWp].transform.position, transform.position) < accuracyWp) //waypointin pozisyonu enemy pozisyonuyla kıyasla
+			{
+				currentWp++;  // circle 
+				if (currentWp >= waypoints.Length) 
+				{
+					currentWp = 0;
+				}
+			}
+
+			//dön bakalım
+			direction = waypoints[currentWp].transform.position - transform.position;  
+			this.transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (direction), rotSpeed * Time.deltaTime);
+			this.transform.Translate (0, 0, Time.deltaTime * speed);
+		}
+
+
+
+
+		if (Vector3.Distance (player.position, this.transform.position) < 10 && (angle < 30 || state == "pursuing")) {  // eğer görebiliyorsam
+			
+			state = "pursuing";
+			this.transform.rotation = Quaternion.Slerp (this.transform.rotation,Quaternion.LookRotation (direction),rotSpeed * Time.deltaTime);
+			
+		
+			anim.SetBool ("isIdle", false);
+			if (direction.magnitude > 5) {
+				
+				this.transform.Translate (0, 0, Time.deltaTime * speed);
+				anim.SetBool ("isWalking", true);
+				anim.SetBool ("isAttacking", false);
+
+			} else {
+			
+				anim.SetBool ("isAttacking", true);
+				anim.SetBool ("isWalking", false);
+		
+			}
+		}
+		else
+		{
+ 
+			anim.SetBool("isWalking", true);
+			anim.SetBool("isAttacking", false);
+			state = "patrol";
+		}
+
+	}
+}
